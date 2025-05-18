264340510741d62dd5f69737b1ecb4f76e35be51
hamed18 <hamedcnec@gmail.com>
2025-05-19 02:55:21 +0600

    Project Setup and added user

---
+++ src/app.ts
@@ -0,0 +1,40 @@
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+import { globalErrorHandler } from './app/middleeatres/globalErrorHandler';
+import userRouter from './app/modules/user/user.router';
+
+const app: Application = express();
+
+//  CORS setup
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "http://localhost:5175",
+    ],
+    credentials: true,
+  })
+);
+
+//parsers
+app.use(express.json());
+
+// application routes
+const getAController = (req: Request, res: Response) => {
+    res.send('Hello World!')
+};
+
+app.get('/', getAController)
+app.use(globalErrorHandler)
+
+// routes
+app.use('api/user', userRouter)
+
+app.use("*", (req: Request, res: Response) =>{
+  res.status(404).json({
+    status: false,
+    message: "Route not found"
+  })
+})
+export default app;
---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,11 @@
+import { USER_ROLE } from "./user.constants";
+
+export interface IUser {
+	name: string;
+	email: string;
+	phoneNumber: string;
+	password: string;
+	role: 'admin' | 'operator';
+	isActive: boolean;
+  }
+  export type TUserRole = keyof typeof USER_ROLE;  
